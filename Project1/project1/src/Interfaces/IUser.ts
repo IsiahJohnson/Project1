cc14b312cc900d14e9fda270abfcb8642aa1683b
IsiahJohnson <92471437+IsiahJohnson@users.noreply.github.com>
2022-05-19 18:41:55 -0400

    create iuser

---
+++ Project1/project1/src/Interfaces/IUser.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    userId: number,
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    userrole: number
+}
